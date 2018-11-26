faa363d057610a35283bba293fd2c9a1d9f65217
Eldson <eldsonlm@gmail.com>
2018-11-26 15:41:54 -0300

    estruturação do projeto

---
+++ src/DAO/CompraDAO.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DAO;
+
+import java.sql.Connection;
+
+/**
+ *
+ * @author aluno
+ */
+public class CompraDAO extends ExecuteSQL{
+    
+    public CompraDAO(Connection con) {
+        super(con);
+    }
+}
